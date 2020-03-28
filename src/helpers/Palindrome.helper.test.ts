07075368c02867d0793fa1ad4a652c522475f003
CForrest97 <craforrest97@gmail.com>
2020-03-28 14:16:24 +0000

    add unit test for trimWord()

--- src/helpers/Palindrome.helper.test.ts
+++ src/helpers/Palindrome.helper.test.ts
@@ -1,16 +1,24 @@
-import { isPalindrome } from './Palindrome.helper';
+import { isPalindrome, trimWord } from './Palindrome.helper';
 
 describe('palindrome helper', () => {
-  it('should return true if word is same backwards', () => {
-    const palindrome = 'racecar';
-    expect(isPalindrome(palindrome)).toBe(true);
+  describe('isPalindrome', () => {
+    it('should return true if word is same backwards', () => {
+      const palindrome = 'racecar';
+      expect(isPalindrome(palindrome)).toBe(true);
+    });
+    it('should return false if word is different backwards', () => {
+      const palindrome = 'abacus';
+      expect(isPalindrome(palindrome)).toBe(false);
+    });
+    it('should ignore spaces', () => {
+      const palindrome = 'a man a plan a canal panama';
+      expect(isPalindrome(palindrome)).toBe(true);
+    });
   });
-  it('should return false if word is different backwards', () => {
-    const palindrome = 'abacus';
-    expect(isPalindrome(palindrome)).toBe(false);
-  });
-  it('should ignore spaces', () => {
-    const palindrome = 'a man a plan a canal panama';
-    expect(isPalindrome(palindrome)).toBe(true);
+  describe('trimWord', () => {
+    it('should remove spaces from word', () => {
+      const palindrome = 'a man a plan a canal panama';
+      expect(trimWord(palindrome)).toBe('amanaplanacanalpanama');
+    });
   });
 });
